8ac886632a1042f403972e0d3f2b82377d752034
Hyun-je <bigae2@gmail.com>
2019-07-01 21:08:08 +0900

    Update show-scroll.js

--- js/show-scroll.js
+++ js/show-scroll.js
@@ -1,14 +1,15 @@
 $(document).ready(function() {
-                  /* 1 */
-                  $(window).scroll( function(){
-                                   /* 2 */
-                                   $('.hideme').each( function(i){
-                                                     var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-                                                     var bottom_of_window = $(window).scrollTop() + $(window).height();
-                                                     /* 3 */
-                                                     if( bottom_of_window > bottom_of_object/2 ){
-                                                     $(this).animate({'opacity':'1'},500);
-                                                     }
-                                                     });
-                                   });
-                  });
+      /* 1 */
+      $(window).scroll( function(){
+           /* 2 */
+           $('.hideme').each( function(i){
+                 var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+                 var bottom_of_window = $(window).scrollTop() + $(window).height();
+                 /* 3 */
+                 if( bottom_of_window > bottom_of_object/2 ){
+                    $(this).animate({'opacity':'1'}, 1500);
+                 }
+                             
+            });
+       });
+  });
