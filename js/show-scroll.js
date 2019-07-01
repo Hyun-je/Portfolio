38097e10e9f176275e81cee0d54fe0048464e1bd
Hyun-je <bigae2@gmail.com>
2019-07-01 20:59:18 +0900

    Add scroll effect

---
+++ js/show-scroll.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+                  /* 1 */
+                  $(window).scroll( function(){
+                                   /* 2 */
+                                   $('.hideme').each( function(i){
+                                                     var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+                                                     var bottom_of_window = $(window).scrollTop() + $(window).height();
+                                                     /* 3 */
+                                                     if( bottom_of_window > bottom_of_object/2 ){
+                                                     $(this).animate({'opacity':'1'},500);
+                                                     }
+                                                     });
+                                   });
+                  });
